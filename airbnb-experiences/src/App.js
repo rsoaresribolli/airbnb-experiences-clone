042207f42c3db42496b3f5a7b757dfbf41ac4dfa
Ricardo Soares Ribolli <ricardo.ribolli@inspira.com.br>
2022-03-01 16:47:21 -0300

    first commit

---
+++ airbnb-experiences/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <header>
+        <h1>App component</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
